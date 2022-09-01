3e6902456b8599a1515201b8796164b3778528f4
Jason Noyes <jrnoyes1@gmail.com>
2022-08-31 19:31:57 -0500

    assignment 5

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,24 @@
+import {useContext} from 'react'
+import {DataContext} from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
 function Gallery(props){
-    return (
+    const data = useContext(DataContext)
+    
+    const display = data.map((item,index) => {
+            return (
         <div>
-            <GalleryItem />
+            <GalleryItem key={index} item={item} />
         </div>
     )
+    })
+    return (
+        <div>
+            {display}
+        </div>
+    )
+
+
 }
 
 export default Gallery
