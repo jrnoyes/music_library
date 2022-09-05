c7011e1a058b3474d74ed636b3ea0ae0366098bc
Jason Noyes <jrnoyes1@gmail.com>
2022-09-05 18:53:15 -0500

    with_context

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
